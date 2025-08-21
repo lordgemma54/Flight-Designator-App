6fda8a5aefd703cd9fbfa44570994dddf7155fbc
Rahul <rtvconcepts@gmail.com>
2025-08-21 12:03:58 -0400

    update file with toString()

---
+++ Module1/SeatReservation.java
@@ -0,0 +1,47 @@
+public class SeatReservation {
+   private String flightDesignator;
+   private java.time.LocalDate flightDate;
+   private String firstName = null;
+   private String lastName = null;
+
+   public String getFlightDesignator() {
+      return flightDesignator;
+   }
+
+   public void setFlightDesignator(String fd) {
+      flightDesignator = fd;
+   }
+
+   public java.time.LocalDate getFlightDate() {
+      return flightDate;
+   }
+
+   public void setFlightDate(java.time.LocalDate date) {
+      flightDate = date;
+   } 
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public void setFirstName(String fn) {
+      firstName = fn;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public void setLastName(String ln) {
+      lastName = ln;
+   }
+
+   @Override
+   public String toString() {
+      return "Seat Reservation {flightDesignator=" + flightDesignator + 
+             ", " + "flightDate=" + flightDate +
+             ", " + "firstName=" + firstName + 
+             ", " + "lastName=" + lastName + "}";
+   
+   }
+}
