515c7e57dfbdb69aa4b98b33ad5f6d838a6dab0d
Rahul <rtvconcepts@gmail.com>
2025-08-21 11:50:51 -0400

    moved to the correct branch

--- SeatReservation.java
+++ SeatReservation.java
@@ -3,4 +3,41 @@ public class SeatReservation {
     private java.time.localDate flightDate;
     private String firstName;
     private String lastName;
+
+    public String getFlightDesignator() {
+        return flightDesignator;
+    }
+
+    public void setFlightDesignator(String fd) {
+        flightDesignator = fd;
+    }
+
+    public java.time.localDate getFlightDate() {
+        return flightDate;
+    }
+
+    public void setFlightDate(java.time.localDate date) {
+        flightDate = date;
+    } 
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String fn) {
+        firstName = fn;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String ln) {
+        lastName = ln;
+    }
+
+    public String toString() {
+        
+
+    }
 }
