c43e574ac3a82c85d79a773ef75e93a517890463
Rahul <rtvconcepts@gmail.com>
2025-08-27 13:44:45 -0400

    files now one directory up

---
+++ Module2/src/main/java/edu/au/cpsc/module2/Launcher.java
@@ -0,0 +1,9 @@
+package edu.au.cpsc.module2;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(SeatReservationApplication.class, args);
+    }
+}
