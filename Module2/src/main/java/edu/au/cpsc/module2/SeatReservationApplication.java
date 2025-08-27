ef6fb5bf3059e97314acd3ade43ac7fdc7059761
Rahul <rtvconcepts@gmail.com>
2025-08-27 14:44:44 -0400

    add a stackPane and reset filePaths to point to the correct project

--- Module2/src/main/java/edu/au/cpsc/module2/SeatReservationApplication.java
+++ Module2/src/main/java/edu/au/cpsc/module2/SeatReservationApplication.java
@@ -1,8 +1,8 @@
 package edu.au.cpsc.module2;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +10,9 @@ import java.io.IOException;
 public class SeatReservationApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SeatReservationApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        FXMLLoader fxmlLoader = new FXMLLoader(SeatReservationApplication.class.getResource("hello-view.fxml"));
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 500, 700);
         stage.setTitle("Rahul Venkatesh's seat reservation App");
         stage.setScene(scene);
         stage.show();
