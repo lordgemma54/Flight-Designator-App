1c2451f4b94c8a07ed9cb98aa52232a726d8f2cf
Rahul <rtvconcepts@gmail.com>
2025-08-27 13:32:39 -0400

    test commit

---
+++ Module-2/Module2/src/main/java/edu/au/cpsc/module2/SeatReservationApplication.java
@@ -0,0 +1,19 @@
+package edu.au.cpsc.module2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SeatReservationApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(SeatReservationApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
