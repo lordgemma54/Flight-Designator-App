acdaeba6d887971eac55b426ebed8d44ee7c3d06
Rahul <rtvconcepts@gmail.com>
2025-09-12 15:28:08 -0400

    added AirlineDatabaseIO, AirlineDatabaseIOTest, AirlineDatabaseTest

--- src/main/java/edu/au/cpsc/module4/FlightDesignator/data/AirlineDatabase.java
+++ src/main/java/edu/au/cpsc/module4/FlightDesignator/data/AirlineDatabase.java
@@ -1,4 +1,31 @@
 package edu.au.cpsc.module4.FlightDesignator.data;
 
-public class AirlineDatabase {
+import edu.au.cpsc.module4.FlightDesignator.model.ScheduledFlight;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AirlineDatabase implements Serializable {
+    private List<ScheduledFlight> flights;
+
+    public AirlineDatabase () {
+        flights = new ArrayList<>();
+    }
+
+    public List<ScheduledFlight> getScheduledFlights() {
+        return flights;
+    }
+
+    public void addScheduledFlight(ScheduledFlight sf) {
+    flights.add(sf);
+    }
+
+    public void removeScheduledFlight(ScheduledFlight sf) {
+    flights.remove(sf);
+    }
+
+    public void updateScheduledFlight(ScheduledFlight sf) {
+
+    }
 }
