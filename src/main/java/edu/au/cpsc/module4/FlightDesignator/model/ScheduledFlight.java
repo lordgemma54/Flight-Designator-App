ad721d76be8a25fef00e1aa6cbfddb924155e2ac
Rahul <rtvconcepts@gmail.com>
2025-09-12 23:33:13 -0400

    finally a functioning UI that displays demo Flights

--- src/main/java/edu/au/cpsc/module4/FlightDesignator/model/ScheduledFlight.java
+++ src/main/java/edu/au/cpsc/module4/FlightDesignator/model/ScheduledFlight.java
@@ -2,6 +2,7 @@ package edu.au.cpsc.module4.FlightDesignator.model;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,30 +15,30 @@ public class ScheduledFlight implements Serializable {
     private java.time.LocalDateTime arrivalTime;
     private HashSet<String> daysOfWeek;
 
+    public static ScheduledFlight sf01;
+    public static ScheduledFlight sf02;
+
     public static List<ScheduledFlight> demoFlights() {
-//        List<ScheduledFlight> flights = new ArrayList<>();
-//        ScheduledFlight sf01 = new ScheduledFlight();
-//        ScheduledFlight sf02 = new ScheduledFlight();
-//
-//        sf01.setFlightDesignator("fx-900");
-//        sf01.setDepartureAirportIdent("KDEN");
-//        sf01.setArrivalAirportIdent("SYR");
-//
-//        sf01.setFlightDesignator("LA-66");
-//        sf01.setDepartureAirportIdent("LAX");
-//        sf01.setArrivalAirportIdent("BLR");
-//
-//        flights.add(sf01);
-//        flights.add(sf02);
-//        return flights;
-        return List.of(new ScheduledFlight("fx-900","KDEN", "SYR" ),
-                new ScheduledFlight("LA-66","LAX", "BLR"));
+        List<ScheduledFlight> flights = new ArrayList<>();
+        sf01 = new ScheduledFlight("k3-256","KDEN",
+                LocalDateTime.of(2025, 9, 12, 9, 15), "SYR",
+                LocalDateTime.of(2025, 9, 12, 11, 30));
+
+        sf02 = new ScheduledFlight("pp-49", "KLAX",
+                (LocalDateTime.of(2025, 9, 12, 10, 45)),"BLR",
+                LocalDateTime.of(2025, 9, 14, 2,25 ));
+
+        flights.add(sf01);
+        flights.add(sf02);
+        return flights;
     }
 
-    public ScheduledFlight(String flightDesignator, String depAirport, String arrAirport) {
+    public ScheduledFlight(String flightDesignator, String depAirport, LocalDateTime depTime, String arrAirport, LocalDateTime arrTime ) {
         setFlightDesignator(flightDesignator);
         setDepartureAirportIdent(depAirport);
+        setDepartureTime(depTime);
         setArrivalAirportIdent(arrAirport);
+        setArrivalTime(arrTime);
     }
     public String getFlightDesignator() {
         return flightDesignator;
@@ -103,5 +104,17 @@ public class ScheduledFlight implements Serializable {
             throw new IllegalArgumentException();
         }
         this.daysOfWeek = new HashSet<>(daysOfWeek);
+    }
+
+    @Override
+    public String toString() {
+        return "ScheduledFlight{" +
+                "flightDesignator='" + flightDesignator + '\'' +
+                ", departureAirportIdent='" + departureAirportIdent + '\'' +
+                ", departureTime=" + departureTime +
+                ", arrivalAirportIdent='" + arrivalAirportIdent + '\'' +
+                ", arrivalTime=" + arrivalTime +
+                ", daysOfWeek=" + daysOfWeek +
+                '}';
     }
 }
--- src/test/java/edu/au/cpsc/module4/FlightDesignator/data/AirlineDatabaseIOTest.java
+++ src/test/java/edu/au/cpsc/module4/FlightDesignator/data/AirlineDatabaseIOTest.java
@@ -15,18 +15,16 @@ class AirlineDatabaseIOTest {
     @Test
     void writeToDatabaseTest() throws IOException, ClassNotFoundException {
         AirlineDatabase database = new AirlineDatabase();
-        ScheduledFlight sf1 = new ScheduledFlight();
-        sf1.setFlightDesignator("k3-256");
-        sf1.setDepartureAirportIdent("KDEN");
-        sf1.setDepartureTime(LocalDateTime.of(2025, 9, 12, 9, 15));
-        sf1.setArrivalAirportIdent("SYR");
+        ScheduledFlight sf1 = new ScheduledFlight("k3-256","KDEN",
+                LocalDateTime.of(2025, 9, 12, 9, 15), "SYR",
+                LocalDateTime.of(2025, 9, 12, 11, 30));
+
         sf1.setDaysOfWeek(Arrays.asList("Sunday", "Tuesday", "Friday"));
 
-        ScheduledFlight sf2 = new ScheduledFlight();
-        sf2.setFlightDesignator("pp-49");
-        sf2.setDepartureAirportIdent("KLAX");
-        sf2.setDepartureTime(LocalDateTime.of(2025, 9, 12, 9, 15));
-        sf2.setArrivalAirportIdent("BLR");
+        ScheduledFlight sf2 = new ScheduledFlight("pp-49", "KLAX",
+                (LocalDateTime.of(2025, 9, 12, 10, 45)),"BLR",
+                LocalDateTime.of(2025, 9, 14, 2,25 ));
+
         sf2.setDaysOfWeek(Arrays.asList("Monday", "Thursday"));
 
         database.addScheduledFlight(sf1);
