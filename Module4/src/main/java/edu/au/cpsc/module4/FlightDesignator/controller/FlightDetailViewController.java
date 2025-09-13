fef3e700a82c749039fc0548be9cd9f89fff2559
Rahul <rtvconcepts@gmail.com>
2025-09-13 17:32:32 -0400

    now functional with two discrete components housed within the main component

--- Module4/src/main/java/edu/au/cpsc/module4/FlightDesignator/controller/FlightDetailViewController.java
+++ Module4/src/main/java/edu/au/cpsc/module4/FlightDesignator/controller/FlightDetailViewController.java
@@ -4,12 +4,37 @@ import edu.au.cpsc.module4.FlightDesignator.model.ScheduledFlight;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+import java.time.format.DateTimeFormatter;
 
 public class FlightDetailViewController {
+
+    @FXML
+    public GridPane flightDetailView;
 
     @FXML
     private TextField flightDesignator, departureAirportIdent, arrivalAirportIdent, arrivalTime, departureTime;
 
     @FXML
     private TableView<ScheduledFlight> flights;
+
+    public void showFlight(ScheduledFlight selectedFlight){
+
+        if(selectedFlight == null) {
+           flightDesignator.clear();
+            departureAirportIdent.clear();
+            arrivalAirportIdent.clear();
+            arrivalTime.clear();
+            departureTime.clear();
+            return;
+        }
+        flightDesignator.setText(selectedFlight.getFlightDesignator());
+        departureAirportIdent.setText(selectedFlight.getDepartureAirportIdent());
+        arrivalAirportIdent.setText(selectedFlight.getArrivalAirportIdent());
+
+        DateTimeFormatter dtFmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        arrivalTime.setText(selectedFlight.getArrivalTime().format(dtFmt));
+        departureTime.setText(selectedFlight.getDepartureTime().format(dtFmt));
+    }
 }
